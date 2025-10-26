0c216cd9cc7c1c144aff6762d497590ffdb22411
Nedy Nugroho <nedynugroho2007@gmail.com>
2025-10-26 19:12:38 +0700

    3. Tugas No 2

---
+++ src/TempatParkir.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TempatParkir {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = 0;
+
+        while (true) {
+            System.out.println("Masukan jenis kendaraan : \n1. Mobil \n2. Motor \n0. Keluar");
+            System.out.print("Pilihan: ");
+
+            int jenis = sc.nextInt();
+            if (jenis == 0) break;
+
+            if (jenis == 1 || jenis == 2) {
+                System.out.print("Masukan Durasi: ");
+                int durasi = sc.nextInt();
+
+                if (durasi > 5) total += 12_500;
+                else if (jenis == 1) total += durasi * 3_000;
+                else total += durasi * 2_000;
+            }
+        }
+
+        System.out.printf("Total pembayaran : Rp.%,d %n", total);
+        System.out.println("Terimakasih telah menggunakan aplikasi ini");
+    }
+}
